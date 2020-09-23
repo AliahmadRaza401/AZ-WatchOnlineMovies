d8e37c004f10218d2752328c68963519441add34
ali raza <aliahmadraza401@gmail.com>
2020-09-23 10:20:34 +0500

    Updates

---
+++ precache-manifest.f228452d1a371bf0757d4ca14c1e42a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b5265d31dd26ed25455f8700d59f07",
+    "url": "/AZ-WatchOnlineMovies/index.html"
+  },
+  {
+    "revision": "ff6a7a120614eabf6f15",
+    "url": "/AZ-WatchOnlineMovies/static/css/main.18852ae9.chunk.css"
+  },
+  {
+    "revision": "fee0506fd006f516ba7d",
+    "url": "/AZ-WatchOnlineMovies/static/js/2.1973da6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AZ-WatchOnlineMovies/static/js/2.1973da6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6a7a120614eabf6f15",
+    "url": "/AZ-WatchOnlineMovies/static/js/main.e2ce4286.chunk.js"
+  },
+  {
+    "revision": "49b64e7f412e51588cf6",
+    "url": "/AZ-WatchOnlineMovies/static/js/runtime-main.711849ae.js"
+  },
+  {
+    "revision": "98317b4e1a1bfa45c4f7ff4989c01423",
+    "url": "/AZ-WatchOnlineMovies/static/media/p1.98317b4e.jpg"
+  },
+  {
+    "revision": "c535fe91caaf73489f612f40bc3c0df0",
+    "url": "/AZ-WatchOnlineMovies/static/media/p2.c535fe91.jpg"
+  },
+  {
+    "revision": "fab0330b3f7424038fe5216347ed05ca",
+    "url": "/AZ-WatchOnlineMovies/static/media/p3.fab0330b.jpg"
+  },
+  {
+    "revision": "f873dccc8893df43713a2c241ca781f0",
+    "url": "/AZ-WatchOnlineMovies/static/media/p6.f873dccc.jpg"
+  }
+]);
